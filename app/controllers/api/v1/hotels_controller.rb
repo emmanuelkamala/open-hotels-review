d1fa03871b1554580ad186981d15c53eb31cd97e
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-27 18:24:15 +0300

    Add logic to the hotel controller

---
+++ app/controllers/api/v1/hotels_controller.rb
@@ -0,0 +1,53 @@
+module Api
+  module V1 
+    class HotelsController < ApplicationController
+      def index 
+        @hotels = Hotel.all 
+
+        render json: HotelSerializer.new(@hotels).serialized_json
+      end
+
+      def show 
+        @hotel = Hotel.find_by(slug: params[:slug])
+
+        render json: HotelSerializer.new(@hotel).serialized_json
+      end
+
+      def create 
+        @hotel = Hotel.new(hotel_params)
+
+        if @hotel.save 
+          render json: HotelSerializer.new(@hotel).serialized_json 
+        else
+          render json: { error: @hotel.errors.messages }, status: 422
+        end
+      end
+
+      def update 
+        @hotel = Hotel.find_by(slug: params[:slug])
+
+        if @hotel.update(hotel_params)
+          render json: HotelSerializer.new(@hotel).serialized_json 
+        else
+          render json: { error: @hotel.errors.messages }, status: 422
+        end
+      end
+
+      def destroy 
+        @hotel = Hotel.find_by(slug: params[:slug])
+
+        if @hotel.destroy 
+          head :no_content 
+        else
+          render json: { error: @hotel.errors.messages }, status: 422
+        end
+      end
+
+      private 
+
+      def hotel_params
+        params.require(:hotel).permit(:name, :image_url)
+      end
+    end
+  end
+end
