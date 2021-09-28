7f6bdead7bbf8b6f6a3f091cd6efb3a5044e889b
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-28 23:27:09 +0300

    Add styles to the header part of hotel component

--- app/javascript/components/Hotel/Header.jsx
+++ app/javascript/components/Hotel/Header.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  padding: 50px 100px 50px 0;
+
+  img {
+    height: 200px;
+    width: 200px;
+    border-radius: 3%;
+    border: 1px solid rgba(0,0,0,0.1);
+    margin-bottom: -64px;
+    margin-right: 10px;
+  }
+
+  h1 {
+    font-size: 30px;
+  }
+`
+const TotalReviews = styled.div`
+  font-size: 18px;
+  padding: 10px 0;
+`
+const TotalOutOf = styled.div`
+  font-size: 18px;
+  font-weight: 400;
+  padding: 10px 0;
+`
 
 const Header = (props) => {
   const { name, image_url, avg_score } = props.attributes
   const total = props.reviews.length
   return (
-    <div className="wrapper">
+    <Wrapper>
       <h1><img src={image_url} alt={name} />{name}</h1>
       <div>
-        <div className="totalReviews">{total} User Reviews</div>
+        <TotalReviews>{total} User Reviews</TotalReviews>
         <div className="starRating"></div>
-        <div className="totalOutOf">{avg_score} out of 5</div>
+        <TotalOutOf>{avg_score} out of 5</TotalOutOf>
       </div>
-    </div>
+    </Wrapper>
   )
 }
 
