c5575cf512725c3ae3c9b6112f64f853edbc616f
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-28 19:16:54 +0300

    Create a header component for the hotel component

--- app/javascript/components/Hotel/Hotel.jsx
+++ app/javascript/components/Hotel/Hotel.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import axios from "axios"
+import React, { useEffect, useState } from "react"
 
-const Hotel = () => {
+const Hotel = (props) => {
+  const [hotel, setHotel] = useState({})
+  const [review, setReview] = useState({})
+
+  useEffect(()=> {
+    const slug = props.match.params.slug
+    const url = `/api/v1/hotels/${slug}`
+
+    axios.get(url)
+      .then(res => setHotel(res.data.data))
+      .catch(error => console.log(error))
+  }, [])
+
   return (
-    <div>
-      Single Hotel
+    <div className="wrapper">
+      <div className="column">
+        <div className="header"></div>
+        <div className="reviews"></div>
+      </div>
+      <div className="column">
+        <div className="review-form">
+          REview form
+        </div>
+      </div>
     </div>
   )
 }
